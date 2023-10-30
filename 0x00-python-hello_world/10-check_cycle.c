cbd8064ba5b364c0497d4d4231add33c917e2866
abel-alx-github <abeltibeso323@gmail.com>
2023-10-30 11:04:36 -0700

    task 10

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,15 +1,22 @@
 #include "lists.h"
+/**
+*check_cycle - check if there is cycle in the singly linked list.
+*@list: pointer to the head of the struct.
+*Return: 0 if there is no cycle, 1 if there is a cycle.
+*/
 int check_cycle(listint_t *list)
 {
-	listint_t **cycle = &list;
-	int i;
+	listint_t *backward = list;
+	listint_t *forward = list;
 
-	for (i = 0; cycle; i++)
+	if (list == NULL || list->next == NULL)
+		return (0);
+	while (forward && backward && backward->next)
 	{
-		if ((*cycle)[i]->next == ((*cycle)[i + 1]->next))
+		forward = forward->next;
+		backward = backward->next->next;
+		if (forward == backward)
 			return (1);
-		*cycle = cycle->next;
 	}
 	return (0);
-
 }
