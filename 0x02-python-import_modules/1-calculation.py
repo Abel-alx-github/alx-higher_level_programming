90dfce66b7a840aa305bdd0b4eca9ded26808272
abel-alx-github <abeltibeso323@gmail.com>
2023-11-02 03:14:58 -0700

    task 1

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+if __name__ == '__main__':
+    a = 10
+    b = 5
+    a_plus_b = add(a, b)
+    a_minus_b = sub(a , b)
+    a_times_b = mul(a, b)
+    a_divide_b = div(a, b)
+    print(f"{a} + {b} = {a_plus_b}")
+    print(f"{a} - {b} = {a_minus_b}")
+    print(f"{a} * {b} = {a_times_b}")
+    print(f"{a} / {b} = {a_divide_b}")
+
