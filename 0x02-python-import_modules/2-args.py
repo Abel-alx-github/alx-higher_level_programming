0b9bd03145342a90a90fd1dddc48fe1d62a55aa7
abel-alx-github <abeltibeso323@gmail.com>
2023-11-02 06:30:42 -0700

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 import sys
 if __name__ == '__main__':
-    argu =len(sys.argv) - 1
+    argu = len(sys.argv) - 1
     if argu == 0:
         print(f"{argu} arguments.")
     elif argu == 1:
         print(f"{argu} argument:\n{1}: {sys.argv[1]}")
     else:
         print(f"{argu} arguments:")
-        for i in range(1, argu +1):
+        for i in range(1, argu + 1):
             print(f"{i}: {sys.argv[i]}")
-
