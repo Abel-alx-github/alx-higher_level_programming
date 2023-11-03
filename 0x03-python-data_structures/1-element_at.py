acb8bc0562541b70c3c51b7b2b32e3dcced47ee2
abel-alx-github <abeltibeso323@gmail.com>
2023-11-03 11:35:51 -0700

    task 1

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,7 @@
+def element_at(my_list, idx):
+    length = len(my_list)
+    if idx >= length or idx < 0:
+        return None
+    for i in range(length):
+        if i == idx:
+            return my_list[i]
