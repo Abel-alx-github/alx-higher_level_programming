8c4371452c118f0c5d46c6dbc0c36ebc7a9d6ea7
abel-alx-github <abeltibeso323@gmail.com>
2023-11-04 00:13:21 -0700

    task 3

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    length = len(my_list)
-    if idx >= length or idx < 0:
+    length = len(my_list) - 1
+    if idx > length or idx < 0:
         cp_list = my_list
         return cp_list
     for i in range(length):
