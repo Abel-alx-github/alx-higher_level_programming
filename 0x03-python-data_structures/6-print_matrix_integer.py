09b37b1994ae9210bb54691918d7309710c42fd9
abel-alx-github <abeltibeso323@gmail.com>
2023-11-05 22:45:18 -0800

    task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in (matrix):
-        new_mat = []
-        new_mat.append(row)
-        for i in new_mat:	       
-            print("{:d}".format(i), end="")
+        print(' '.join("{:d}".format(elm) for elm in row))
