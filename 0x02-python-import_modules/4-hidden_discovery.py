0c34c253c60cc9f939053aac73c97ac887eff849
abel-alx-github <abeltibeso323@gmail.com>
2023-11-07 05:30:20 -0800

    task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,14 +1,7 @@
 #!/usr/bin/python3
-import dis
-import imp
 if __name__ == '__main__':
-    module = imp.load_source("hidden_4", "hidden_4.pyc")
-    inst = dis.get_instructions(module)
-    names = set()
-    for ins in inst:
-        if ins.opcod == dis.opmap["LOAD_NAME"]:
-            name = ins.arg
-            if not name.startswith("__"):
-                names.add(name)
-    for name in sorted(names):
-        print(name)
+    import hidden_4
+    words = dir(hidden_4)
+    for name in words:
+        if name[:2] != "__":
+            print(name)
