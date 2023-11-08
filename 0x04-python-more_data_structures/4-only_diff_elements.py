6dadd534b1e317912b07a5146b0d2f28e486315a
abel-alx-github <abeltibeso323@gmail.com>
2023-11-08 07:57:10 -0800

    task 4

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    return set_1.union(set_2)
+    combine = set_1.union(set_2)
+    for char in set_1:
+        if char in set_2:
+            combine.remove(char)
+    return combine
