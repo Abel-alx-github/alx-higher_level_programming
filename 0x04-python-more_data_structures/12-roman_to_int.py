08f528adf1c3b0c7aa8ed199d91cae9618b1a577
abel-alx-github <abeltibeso323@gmail.com>
2023-11-09 00:24:10 -0800

    task 12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not roman_string or roman_string is None:
+        return 0
+
+    dic_key_value = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    }
+    length = len(roman_string) - 1
+    intger = 0
+    for i in range(length, -1, -1):
+        last_r = dic_key_value[roman_string[i]]
+        if i < length and last_r < dic_key_value[roman_string[i + 1]]:
+            intger -= last_r
+        else:
+            intger += last_r
+    return intger
