b3ad4422bca91e3dd22ffa81684426d5b96ff635
abel-alx-github <abeltibeso323@gmail.com>
2023-11-09 02:52:19 -0800

    task 102

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -3,4 +3,3 @@ def complex_delete(a_dictionary, value):
     new_dic = {key: val for key, val in a_dictionary.items() if val != value}
     if value in a_dictionary.values():
         return new_dic
-    return a_dictionary
