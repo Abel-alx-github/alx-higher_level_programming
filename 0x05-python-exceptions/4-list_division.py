3ebae5f0ee5c996a309cf1ab4b26c45881ee7e0c
abel-alx-github <abeltibeso323@gmail.com>
2023-11-20 10:41:31 -0800

    task- 4

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -9,6 +9,7 @@ def list_division(my_list_1, my_list_2, list_length):
             div = 0
         except IndexError:
             print("out of range")
+            div = 0
         except ZeroDivisionError:
             div = 0
             print("division by 0")
