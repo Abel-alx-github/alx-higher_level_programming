568502953669de0cf3902b751fe46eaef61e0280
abel-alx-github <abeltibeso323@gmail.com>
2023-11-20 08:54:44 -0800

    task- 1

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,6 +3,6 @@ def safe_print_integer(value):
     val = True
     try:
         print("{:d}".format(value))
-    except ValueError:
+    except (ValueError, TypeError):
         val = False
     return (val)
