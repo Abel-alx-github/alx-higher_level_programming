7f07631562aff8c1409081db2b7624470f653afd
abel-alx-github <abeltibeso323@gmail.com>
2023-11-21 12:37:14 -0800

    task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Define class named as square"""
+
+
+class Square:
+    """block of this class, statment"""
+    def __init__(self, size=0):
+        """ initialize the instance or object of this class.
+        Args:
+            size(int): size of new instance.
+        """
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
+
+    def area(self):
+        return self.__size**2
