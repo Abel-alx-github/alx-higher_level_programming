138a8f66b0879a28f0be4df1c62b6757d674e19d
abel-alx-github <abeltibeso323@gmail.com>
2023-11-22 06:56:10 -0800

    task 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+
+class Square:
+    ''' represent square class, statment'''
+
+    def __init__(self, size):
+        ''' initailize the object'''
+        self.size = size
+
+    @property
+    def size(self):
+        ''' Args:
+            size(int): method to retrive value
+        Return: returns value of size'''
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        ''' Args:
+            size(int): method to set value, setter
+            '''
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        ''' Args:
+            self: object
+            Return: area of square'''
+        return self.__size ** 2
