1eb60767300b02107727647638f5dfbf9eaaf51d
abel-alx-github <abeltibeso323@gmail.com>
2023-11-27 08:02:01 -0800

    task 2

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -43,6 +43,6 @@ class Rectangle():
 
     def perimeter(self):
         """ return pwrimeter of rectangle"""
-        if width == 0 or height == 0:
+        if self.__width == 0 or self.__height == 0:
             return 0
         return 2 * self.__height + 2 * self.__width
