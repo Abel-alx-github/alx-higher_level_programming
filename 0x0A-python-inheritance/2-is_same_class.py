a303e94c14ea0a7ebbb9583aee5cde4c1bfdeb05
abel-alx-github <abeltibeso323@gmail.com>
2023-12-04 07:02:42 -0800

    task 2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" module contanis function defination"""
+
+
+def is_same_class(obj, a_class):
+    """ represent function defination """
+
+    if type(obj) == a_class:
+        return True
+    else:
+        return False
