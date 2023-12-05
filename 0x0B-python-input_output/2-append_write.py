f7bb0922b2c97ef03613551a32ffea8605fbd1a1
abel-alx-github <abeltibeso323@gmail.com>
2023-12-05 03:35:21 -0800

    task 2

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -5,5 +5,6 @@
 def append_write(filename="", text=""):
     """ function that append text to file"""
     with open(filename, mode="a", encoding="utf-8") as a_file:
+        b = a_file.tell()
         a_file.write(text)
-        return a_file.tell()
+        return a_file.tell() - b
