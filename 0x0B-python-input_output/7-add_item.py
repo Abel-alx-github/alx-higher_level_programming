3edfb1f67fc9855c423c5a7659511fedbf123840
abel-alx-github <abeltibeso323@gmail.com>
2023-12-05 11:22:16 -0800

    taks 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,9 +8,9 @@ if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
     load_from_json_file = __import__('6-load_from_json_file')\
         .load_from_json_file
-
-    args = []
-    for arg in sys.argv[1:]:
-        args.append(arg)
-    save_to_json_file(args, "add_item.json")
-    load_from_json_file("add_item.json")
+    try:
+        old_list = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        old_list = []
+    old_list.extend(sys.argv[1:])
+    save_to_json_file(old_list, "add_item.json")
