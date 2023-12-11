44ee6c20d02c7b7aeb508a851a2f7c4a5e478122
abel-alx-github <abeltibeso323@gmail.com>
2023-12-11 10:44:18 -0800

    almost_circle

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+""" module contains class defination"""
+import json
+
+
+class Base:
+    """ represent clase Base"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """ returns the JSON string representation of list_dictionaries """
+        if not list_dictionaries or list_dictionaries == []:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """ writes the JSON string representation of list_objs to a file:"""
+        name = cls.__name__ + ".json"
+        if list_objs is None:
+            with open(name, "w") as f:
+                f.writelines([])
+        elif list_objs:
+            with open(name, "w", encoding="utf-8") as f:
+                dic_list = [listt.to_dictionary() for listt in list_objs]
+                f.write(Base.to_json_string(dic_list))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """ that returns the list of the JSON string
+            representation json_string:"""
+        if not json_string or json_string is None:
+            return "[]"
+        else:
+            return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """returns an instance with all attributes already set"""
+        if dictionary and dictionary != {}:
+            if cls.__name__ == "Rectangle" or cls.__name__ == "Square":
+                dummy = cls(10, 20, 30, 40)
+                dummy.update(**dictionary)
+                return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        """returns a list of instances:"""
+        fname = cls.__name__ + ".json"
+        try:
+            with open(fname, mode="r", encoding="utf-8") as jf:
+                dictt = Base.from_json_string(jf.read())
+                return [cls.create(**dic) for dic in dictt]
+        except IOError:
+            return "[]"
