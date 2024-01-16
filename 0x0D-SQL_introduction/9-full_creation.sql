69ebfcc572e5e8f60c82f51901491a27438c7b86
abel-alx-github <abeltibeso323@gmail.com>
2024-01-16 13:08:01 -0800

    mysql

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,5 +1,4 @@
 -- creates a table second_table
 
-CREATE TABLE IF NOT EXISTS `second_table` (`id` INT, `name` VARCHAR(256), `score` INT);
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "John", 10),
-       (1, "Alex", 3), (2, "Bob", 14), (3, "George", 8);
+CREATE TABLE IF NOT EXISTS `second_table` (`id` INT, `name` VARCHAR(256), `score` INT)
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "John", 10), (1, "Alex", 3), (2, "Bob", 14), (3, "George", 8);
