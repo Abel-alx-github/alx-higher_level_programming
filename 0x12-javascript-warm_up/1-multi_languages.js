ba927d0983c6b45c093e722829672099064ae192
abel-alx-github <abeltibeso323@gmail.com>
2024-02-12 08:28:34 -0800

    task 1

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -8,9 +8,9 @@ The third line: “JavaScript is amazing”
 You must use console.log(...) to print all output
 You are not allowed to use var
 */
-const line1 = "C is fun";
-const line2 = "Python is cool";
-const line3 = "JavaScript is amazing";
+const line1 = 'C is fun';
+const line2 = 'Python is cool';
+const line3 = 'JavaScript is amazing';
 
 console.log(line1);
 console.log(line2);
