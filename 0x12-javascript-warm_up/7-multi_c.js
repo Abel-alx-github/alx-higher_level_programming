9ff78f11f0bac081f183ee51bf5f6a3d6406d42f
abel-alx-github <abeltibeso323@gmail.com>
2024-02-12 11:48:34 -0800

    task 7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,14 +1,12 @@
 #!/usr/bin/node
 
-const c_is = "C is fun";
-
 const argv = process.argv.slice(2);
 const intNum = Number(argv[0]);
 let i = 0;
 
 if (!isNaN(intNum)) {
   while (i++ < intNum) {
-    console.log(c_is);
+    console.log('C is fun');
   }
 } else {
   console.log('Missing number of occurrences');
