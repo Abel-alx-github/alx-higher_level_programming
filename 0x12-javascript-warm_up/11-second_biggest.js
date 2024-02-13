3bde6e42a9b424e67c9a05c6935d4ff788a29ee4
abel-alx-github <abeltibeso323@gmail.com>
2024-02-13 02:13:27 -0800

    task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
 
 let args = process.argv.slice(2);
-args = args.map(arg => parseInt(arg, 10));
+args = args.map(Number);
 args = args.sort();
 
 if (args.length < 2) {
-  console.log(0);
+  console.log('0');
 } else {
+  console.log(args);
   console.log(args[args.length - 2]);
 }
