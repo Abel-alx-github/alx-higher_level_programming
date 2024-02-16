0902c77ad4dc4124ddecbbf385c34d1bc9a1b9a6
abel-alx-github <abeltibeso323@gmail.com>
2024-02-16 01:36:51 -0800

    task 1, 2

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" module that connect ot detabase and fetchall cities'"""
+
+
+import sys
+import MySQLdb
+
+
+if __name__ == '__main__':
+    myDb = MySQLdb.connect(
+        host='localhost',
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        database=sys.argv[3], port=3306)
+
+    myCursor = myDb.cursor()
+
+    sql = f"SELECT * FROM cities ORDER BY cities.id ASC"
+    myCursor.execute(sql)
+
+    all_states = myCursor.fetchall()
+    for each in all_states:
+        print(each)
+
+    myCursor.close()
+    myDb.close()
