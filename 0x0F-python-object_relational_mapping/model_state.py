b37314b4a032b79b91978bd9a62ce473906bdc86
abel-alx-github <abeltibeso323@gmail.com>
2024-02-16 11:25:31 -0800

    task 6

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,16 +1,13 @@
 #!/user/bin/python3
 ''' create state class that inherits methods form sqlalchamey'''
 
-
-from sqlalchamy import Column, Integer, String, MetaData
-from sqlalchamy.ext.orm import declarative_base
-
-mainData = MetaData()
-
-Base = declarative_base(metadata=mainData)
+from sqlalchemy import Column, Integer, String, MetaData
+from sqlalchemy.ext.declarative import declarative_base
+maindata = MetaData()
+Base = declarative_base(metadata=maindata)
 
 
 class State(Base):
     __tablename__ = 'states'
-    id = column(Integer, unique=True, primary_key=True, nualleable=False)
-    name = column(String(128), nualleable=False)
+    id = Column(Integer, unique=True, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
