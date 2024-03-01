12b43cdfd3ffec3cc8c22a9f61cfbc0362fa0ec3
abel-alx-github <abeltibeso323@gmail.com>
2024-03-01 06:41:07 -0800

    find peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,8 +1,22 @@
 #!/usr/bin/python3
-#find the pick from the given list
+# find the pick from the given list
 
 def find_peak(list_of_integers):
-  if list_of_integers is None:
+    if list_of_integers == []:
+        return None
+    high = len(list_of_integers)
+    if high == 1:
+        return list_of_integers[0]
+    if high == 2:
+        return max(list_of_integers)
+
+    mid = high // 2
+    if ((list_of_integers[mid] > list_of_integers[mid - 1]) and
+       (list_of_integers[mid] > list_of_integers[mid + 1])):
+        return list_of_integers[mid]
+    elif list_of_integers[mid] < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
+    else:
+        return find_peak(list_of_integers[mid + 1:])
+
     return None
-  if list_of_integers:
-    return max(list_of_integers)
