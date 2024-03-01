2cbc910f34ed6eb67e9be6cdc79630583af7c537
abel-alx-github <abeltibeso323@gmail.com>
2024-03-01 02:09:52 -0800

    curl print if status line is 200

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 #sends a GET request to the URL, and displays the body of the response
-
-code=$(curl -sI "$1" | awk '/HTTP/ {print $2}')
-if [[ "$code" == "200" ]]; then
-  curl -sL "$1" 
-fi
+curl -sL "$1" 
