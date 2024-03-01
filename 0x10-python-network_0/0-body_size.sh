5f0e641a69ea72c7a8a66a4a3e2cb8a2ad65a777
abel-alx-github <abeltibeso323@gmail.com>
2024-03-01 01:08:50 -0800

    curl to look content_length

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,10 +1,4 @@
 #!/usr/bin/bash
 # takes in a URL, sends a request to that URL, and displays the size of the body of the response
-
-if [ "$#" == 0 ]; then
-  echo "usage: ./0-body_size.sh ip"
-  exit 1
-fi
-
 url="$1"
-curl -Is "$url" | awk '/Content-Length:/ {print $2}'
+curl -s -I "$url" | awk '/Content-Length:/ {print $2}'
