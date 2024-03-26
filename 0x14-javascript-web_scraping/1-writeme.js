f6e0dc2bc61e4c8143134d8ba400148a05770dad
abel-alx-github <abeltibeso323@gmail.com>
2024-03-26 06:26:59 -0700

    write to fill with fs and process.argv

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -10,7 +10,7 @@ If an error occurred during while writing, print the error object
 
 const fs = require('fs');
 
-fs.writeFile(process.argv[2], process.argv[2], 'utf-8', (err) => {
+fs.writeFile(process.argv[2], process.argv[3], 'utf-8', (err) => {
   if (err) {
     console.error(err);
   }
