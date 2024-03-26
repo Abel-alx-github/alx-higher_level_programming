b4558c3201ef45b1c00b3cea41eeb2d76fa6aa8b
abel-alx-github <abeltibeso323@gmail.com>
2024-03-26 06:25:11 -0700

    write to fill with fs and process.argv

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -13,7 +13,5 @@ const fs = require('fs');
 fs.writeFile(process.argv[2], process.argv[2], 'utf-8', (err) => {
   if (err) {
     console.error(err);
-    return;
   }
-  console.log('successfully writen to file');
 });
